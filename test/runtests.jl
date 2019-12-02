7ca5099cf3ba34e74ae0e70517f597342d9ca7cc
Alexander Seiler <seileralex@gmail.com>
2019-12-02 23:26:51 +0100

    Use testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
 using AdventOfCode2019
 using Test
 
-@test AdventOfCode2019.day01() == [3442987, 5161601]
-@test AdventOfCode2019.day02() == [3101844, 8478]
+@testset "Day 1" begin
+    @test AdventOfCode2019.day01() == [3442987, 5161601]
+end
+
+@testset "Day 2" begin
+    @test AdventOfCode2019.day02() == [3101844, 8478]
+end
