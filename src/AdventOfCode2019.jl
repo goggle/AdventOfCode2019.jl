aafccf8994fbaeeea00661c7d159a03810722cb1
Alexander Seiler <seileralex@gmail.com>
2019-12-04 04:02:30 +0100

    Add Printf

--- src/AdventOfCode2019.jl
+++ src/AdventOfCode2019.jl
@@ -1,5 +1,8 @@
 module AdventOfCode2019
 
+using BenchmarkTools
+using Printf
+
 include(joinpath(@__DIR__, "day01", "day01.jl"))
 include(joinpath(@__DIR__, "day02", "day02.jl"))
 include(joinpath(@__DIR__, "day03", "day03.jl"))
@@ -13,6 +16,15 @@ end
 
 function inputPath(fname::String)
     return joinpath(splitpath(@__DIR__)[1:end-1]..., "data", fname)
+end
+
+function benchmark()
+    for day in 1:25
+        sym = Symbol("day" * @sprintf("%02d", day))
+        !isdefined(AdventOfCode2019, sym) && continue
+        func = getproperty(AdventOfCode2019, sym)
+        @btime AdventOfCode2019.func()
+    end
 end
 
 end # module
