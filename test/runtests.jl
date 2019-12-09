4fbed2501f0a35539efc6438a71fce1cdedb908c
Alexander Seiler <seileralex@gmail.com>
2019-12-09 16:05:42 +0100

    Day 7: Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -62,3 +62,22 @@ end
     @test AdventOfCode2019.Day06.day06("COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN") == [54, 4]
     @test AdventOfCode2019.Day06.day06() == [194721, 316]
 end
+
+@testset "Day 7" begin
+    inp = parse.(Int, split("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0", ","))
+    @test AdventOfCode2019.Day07.run_amplifiers(inp, [4, 3, 2, 1, 0], 0) == 43210
+
+    inp = parse.(Int, split("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0", ","))
+    @test AdventOfCode2019.Day07.run_amplifiers(inp, [0, 1, 2, 3, 4], 0) == 54321
+
+    inp = parse.(Int, split("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0", ","))
+    @test AdventOfCode2019.Day07.run_amplifiers(inp, [1, 0, 4, 3, 2], 0) == 65210
+
+    inp = parse.(Int, split("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5", ","))
+    @test AdventOfCode2019.Day07.run_feedback_loop(inp, [9, 8, 7, 6, 5], 0) == 139629729
+
+    inp = parse.(Int, split("3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10", ","))
+    @test AdventOfCode2019.Day07.run_feedback_loop(inp, [9, 7, 8, 5, 6], 0) == 18216
+
+    @test AdventOfCode2019.Day07.day07() == [844468, 4215746]
+end
